e956ca5ae4d55f39b6a9a55c103b082ba8333afb
Martin Röbke <martin.roebke@web.de>
2024-07-04 00:57:34 +0200

    Update portal64neo4j.py
    Add _read_seasons function

--- portal64neo4j/portal64neo4j.py
+++ portal64neo4j/portal64neo4j.py
@@ -1,9 +1,9 @@
 import logging
 from pathlib import Path
 import sys
-from typing import List
+from typing import Any, Dict, List
 from dotenv import dotenv_values
-from neo4j import GraphDatabase
+from neo4j import GraphDatabase, Record
 
 from utilities import get_parser, logging_cfg
 
@@ -23,12 +23,19 @@ def main(args: List[str]) -> None:
         config["NEO4J_SERVERURL"], config["NEO4J_USER"], config["NEO4J_PASSWORD"]
     )
 
+    seasons = app.get_seasons()
+    for season in sorted(seasons, key=lambda s: s["saison"]["jahr"]):
+        print(season)
+
+    del app
+
 
 class Portal64Neo4j:
     def __init__(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
+        self.driver.verify_connectivity()
         LOGGER.info(
-            f"{self.__class__.__name__} started driver {self.driver.__class__.__name__} "
+            f"{self.__class__.__name__} started driver {self.driver.__class__.__name__} at {self.driver.default_targets} "
         )
 
     def __del__(self):
@@ -37,6 +44,17 @@ class Portal64Neo4j:
         LOGGER.info(
             f"{self.__class__.__name__} closed driver {self.driver.__class__.__name__}"
         )
+
+    @staticmethod
+    def _read_seasons(tx) -> List[Record]:
+        # query = "MATCH (n:Saison) RETURN n.name as saison"
+        query = "MATCH (n:Saison) RETURN n as saison"
+        result = tx.run(query)
+        return [row for row in result]
+
+    def get_seasons(self) -> List[Record]:
+        with self.driver.session(database="neo4j") as session:
+            return session.read_transaction(self._read_seasons)
 
 
 def init():
